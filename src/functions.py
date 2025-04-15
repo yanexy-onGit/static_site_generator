aeb4758e2c52e9c05e1c85fe112a28921b3eb022
nexy <nexyvanessa@gmail.com>
2025-04-15 20:37:17 +0200

    write out markdown_to_html_node function as well as necessary helper functions: text_to_children, strip_block_denotation_md; and corresponding tests

--- src/functions.py
+++ src/functions.py
@@ -1,9 +1,10 @@
 from functools import reduce
 from enum import Enum
-from re import match, fullmatch, findall, split as regex_split
+from re import match, fullmatch, sub, findall, split as regex_split
 
 from textnode import TextType, TextNode
 from leafnode import LeafNode
+from parentnode import ParentNode
 
 class TagType():
     NO_TAG = None
@@ -14,13 +15,15 @@ class TagType():
     IMG = "img"
 
 class BlockType(Enum):
-    P = "paragraph"
-    H = "heading"
+    P = "p"
+    H = "h"
     CODE = "code"
     BQ = "blockquote"
-    UL = "unordered_list"
-    OL = "ordered_list"
+    UL = "ul"
+    OL = "ol"
 
+
+# for text_to_children function
 def text_node_to_html_node(text_node):
     return_dict_plain = {
         TextType.PLAIN: TagType.NO_TAG,
@@ -29,7 +32,10 @@ def text_node_to_html_node(text_node):
         TextType.CODE: TagType.CODE,
     }
     def get_plain_leaf(text_type):
-        return LeafNode(return_dict_plain[text_type], text_node.text)
+        text = text_node.text
+        if text_type != TextType.CODE:
+            text = sub(r"\s+", " ", text_node.text)
+        return LeafNode(return_dict_plain[text_type], text)
     if text_node.text_type in return_dict_plain.keys():
         return get_plain_leaf(text_node.text_type)
     match text_node.text_type:
@@ -39,6 +45,7 @@ def text_node_to_html_node(text_node):
             return LeafNode(TagType.IMG, None, props={ "src": text_node.url, "alt": text_node.text })
     raise Exception("text_node.text_type invalid")
 
+# for text_to_textnodes function
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     def split_one_node(node):
         if node.text_type == TextType.IMG:
@@ -56,12 +63,15 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
         return return_nodes
     return reduce(lambda a, b: a + b, [split_one_node(node) for node in old_nodes])
 
+# for split_nodes_image function > for text_to_textnodes function
 def extract_markdown_images(md):
     return findall(r"!\[([^\]]*)\]\(([^\)]+)\)", md)
 
+# for split_nodes_link function > for text_to_textnodes function
 def extract_markdown_links(md):
     return findall(r"(?<!!)\[([^\]]*)\]\(([^\)]+)\)", md)
 
+# for text_to_textnodes function
 def split_nodes_link(old_nodes):
     def split_one_node(node):
         return_nodes = []
@@ -77,6 +87,7 @@ def split_nodes_link(old_nodes):
         return return_nodes
     return reduce(lambda a, b: a+b, [split_one_node(node) for node in old_nodes])
 
+# for text_to_textnodes function
 def split_nodes_image(old_nodes):
     def split_one_node(node):
         return_nodes = []
@@ -92,6 +103,7 @@ def split_nodes_image(old_nodes):
         return return_nodes
     return reduce(lambda a, b: a+b, [split_one_node(node) for node in old_nodes])
 
+# for text_to_children function
 def text_to_textnodes(text):
     nodes = split_nodes_link(split_nodes_image([TextNode(text, TextType.PLAIN)]))
     text_type_delim_dict = {
@@ -103,9 +115,11 @@ def text_to_textnodes(text):
         nodes = split_nodes_delimiter(nodes, delim, text_t)
     return nodes
 
+# for markdown_to_html_node function
 def markdown_to_blocks(md):
     return list(filter(lambda string: len(string), ["\n".join([line.strip() for line in block.strip().split("\n")]) for block in md.split("\n\n")]))
 
+# for markdown_to_html_node function
 def block_to_block_type(md_block):
     if match(r"#{1,6}\s+\S", md_block) != None:
         return BlockType.H
@@ -118,4 +132,44 @@ def block_to_block_type(md_block):
     if fullmatch(r"[1-9]\d*\. .*(?:\n[1-9]\d*\. .*)*", md_block) != None:
         return BlockType.OL   
     return BlockType.P
-    
+
+# for markdown_to_html_node function
+def text_to_children(text):
+    return [text_node_to_html_node(node) for node in text_to_textnodes(text)]
+
+# for markdown_to_html_node function
+def strip_block_denotation_md(block, type):
+    match type:
+        case BlockType.P:
+            return block
+        case BlockType.OL:
+            return sub(r"\n[1-9]\d*\. +", "\n", sub(r"^[1-9]\d*\. +", "", block))
+        case BlockType.UL:
+            return sub(r"\n- +", "\n", sub(r"^- +", "", block))
+        case BlockType.BQ:
+            return sub(r"\n>", "\n", sub(r"^>", "", block))
+        case BlockType.CODE:
+            return sub(r"^```\n", "", sub(r"\n```$", "", block))
+        case BlockType.H:
+            return sub(r"^#{1,6}\s+", "", block)
+    raise Exception("invalid type (block_type)")
+
+def markdown_to_html_node(md):
+    md_blocks = markdown_to_blocks(md)
+    main_node_children = []
+    for block in md_blocks:
+        block_type = block_to_block_type(block)
+        html_tag = block_type.value
+        if block_type==BlockType.H:
+            html_tag += str(len(match(r"#{1,6}", block).group()))
+        text = strip_block_denotation_md(block, block_type)
+        html_block_children = []
+        if block_type==BlockType.OL or block_type==BlockType.UL:
+            html_block_children = [ParentNode(tag="li", children=text_to_children(bullet_text)) for bullet_text in text.split("\n")]
+        elif block_type==BlockType.CODE:
+            main_node_children.append(ParentNode("pre", [text_node_to_html_node(TextNode(text, TextType.CODE))]))
+            continue
+        else:
+            html_block_children = text_to_children(text)
+        main_node_children.append(ParentNode(tag=html_tag, children=html_block_children))
+    return ParentNode("div", children=main_node_children)      
--- src/test_functions.py
+++ src/test_functions.py
@@ -180,6 +180,90 @@ class TestFunctions(unittest.TestCase):
             self.assertEqual(block_to_block_type(ol), BlockType.OL)
         for no_ol in bad_ol:
             self.assertEqual(block_to_block_type(no_ol), BlockType.P)
+    def test_strip_block_denotation_md(self):
+        blocks_to_denotate = [
+            (" nothing more than\na paragraph", BlockType.P),
+            ("1. one line\n3.  second line\n4807. 1\n6.  ", BlockType.OL),
+            ("- one line\n-  second line\n- 1\n-  ", BlockType.UL),
+            (">something\n> and more\n>and so on", BlockType.BQ),
+            ("> also fiiine", BlockType.BQ),
+            ("```\nbla\n     something\n```", BlockType.CODE),
+            ("## h2 heading", BlockType.H),
+            ("######      6th level heading", BlockType.H),
+        ]
+        expected_returns = [
+            " nothing more than\na paragraph",
+            "one line\nsecond line\n1\n",
+            "one line\nsecond line\n1\n",
+            "something\n and more\nand so on",
+            " also fiiine",
+            "bla\n     something",
+            "h2 heading",
+            "6th level heading",
+        ]
+        returns = [strip_block_denotation_md(*inp_tup) for inp_tup in blocks_to_denotate]
+        self.assertListEqual(returns, expected_returns)
+    def test_paragraphs(self):
+        md = """
+            This is **bolded** paragraph
+            text in a p
+            tag here
+
+            This is another paragraph with _italic_ text and `code` here
+
+            """
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><p>This is <b>bolded</b> paragraph text in a p tag here</p><p>This is another paragraph with <i>italic</i> text and <code>code</code> here</p></div>",
+        )
+    def test_codeblock(self):
+        md = """
+            ```
+            This is text that _should_ remain
+            the **same** even with inline stuff
+            ```
+            """
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        expected_html = "<div><pre><code>This is text that _should_ remain\nthe **same** even with inline stuff</code></pre></div>"
+        # print(f"html as returned:\n{html}")
+        # print(f"html as expected:\n" + expected_html)
+        self.assertEqual(
+            html,
+            expected_html
+        )
+    def test_plentiful(self):
+        md =(
+            """
+            # first heading
+
+            ## 2nd heading
+
+            ####### first paragraph
+            and it goes on.
+
+            and there is some inline `code` and nothing else
+            (inside a paragraph)
+
+            >but we can
+            >also write
+            >block quotes
+
+            2. i think
+            2. this turned out
+            4. rather well
+            666.  what do you say ?
+
+            - but lists exist also like so
+            - right?
+        
+            """)
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        print(f"html as returned:\n{html}")
+            
         
         
 
