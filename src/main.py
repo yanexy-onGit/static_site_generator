9bc5798cf511f0989434afe46bf55c4880e3144c
nexy <nexyvanessa@gmail.com>
2025-04-16 21:47:48 +0200

    attempt swapping / for _blank so that the webpages resources might be properly accessed

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ def main():
     basepath = "/"
     dest_dir_path = "public"
     if len(argv)>1:
-        # basepath = argv[1]
+        basepath = ""
         dest_dir_path = "docs"
     mv_contents_static_to_(dest_dir_path)
     generate_pages_recursive("content", dest_dir_path=dest_dir_path, basepath=basepath)
